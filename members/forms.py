5a5afa65b7d366020e3d1a7e46d99ad71163445c
henryib <ugochukwu.ui@outlook.com>
2023-03-15 00:08:24 +0000

    number issue

--- members/forms.py
+++ members/forms.py
@@ -18,7 +18,7 @@ class memberform(forms.ModelForm):
         widgets = {'first_name': forms.TextInput(attrs={'class': 'form-control'})
                   , 'last_name': forms.TextInput(attrs={'class': 'form-control'})
                   , 'email': forms.EmailInput(attrs={'class': 'form-control'})
-                  , 'phone_number':  forms.NumberInput(attrs={'class': 'form-control'})
+                  , 'phone_number': forms.NumberInput(attrs={'class': 'form-control'})
                   , 'hop': forms.TextInput(attrs={'class': 'form-control'})
                   , 'date_joined': forms.DateInput(attrs={'class': 'form-control'})
                   , 'occupation': forms.TextInput(attrs={'class': 'form-control'})
